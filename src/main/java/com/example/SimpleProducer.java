15c20375e3db2b1e9893a187dcb664e1676626dc
jungwon <mac2001035@gmail.com>
2024-10-06 01:54:58 +0900

    FEAT: Record 전송.

--- src/main/java/com/example/SimpleProducer.java
+++ src/main/java/com/example/SimpleProducer.java
@@ -1,10 +1,13 @@
 package com.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.util.Properties;
 
+@Slf4j
 public class SimpleProducer {
     private final static String TOPIC_NAME = "test";
     private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
@@ -16,5 +19,12 @@ public class SimpleProducer {
         configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+        String messageValue = "testMessage";
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, messageValue);
+        producer.send(record);
+        log.info("{}",record);
+        producer.flush();
+        producer.close();
     }
 }
