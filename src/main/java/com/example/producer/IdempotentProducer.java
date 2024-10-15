7ec213cfa3c1e3793de0da26b1caca98cc620a7e
jungwon <mac2001035@gmail.com>
2024-10-15 19:37:59 +0900

    FEAT: 멱등성 프로듀서 설정.

---
+++ src/main/java/com/example/producer/IdempotentProducer.java
@@ -0,0 +1,32 @@
+package com.example.producer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+@Slf4j
+public class IdempotentProducer {
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME,"Jungwon","31");
+        producer.send(record);
+        ProducerRecord<String, String> record2 = new ProducerRecord<>(TOPIC_NAME,"HeeEun","21");
+        producer.send(record2);
+        producer.flush();
+        producer.close();
+    }
+}
